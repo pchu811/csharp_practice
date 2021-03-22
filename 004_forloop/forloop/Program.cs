787ab41cf46c16b5083eec9341e17ad0e8183bf8
PeiChi Chu <pchu2@go.shoreline.com>
2021-03-22 23:52:41 +0800

    forloop practice

---
+++ 004_forloop/forloop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 004_forloop/forloop2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace forloop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for ( int x = 10; x >= 0; x--)
+            {
+                Console.WriteLine(x);
+            }
+
+            for (; ; )
+            {
+                Console.WriteLine("Forever Love to 啾啾");
+            }
+            
+
+            Console.Read();
+        }
+    }
+}
